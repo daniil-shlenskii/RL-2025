bae96b3861e88edc415ed3baeafff05b8e94b4d7
markbc7 <adirock170595@gmail.com>
2025-03-03 22:21:36 +0300

    Added Trex jumper env and other code

--- src/envs/custom_envs/__init__.py
+++ src/envs/custom_envs/__init__.py
@@ -1,3 +1,10 @@
 from .cardenv_1d import CardEnv
 from .obstacles import Simple1DPathEnv
 from .point import PointEnv
+from gymnasium.envs.registration import register
+
+register(
+    id="TRex-v0",
+    entry_point="src.envs.custom_envs.trex_env:TRexEnv",
+    max_episode_steps=1000,
+)
